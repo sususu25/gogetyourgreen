b847a624dd02b198fdfb57661186da88e8fd4c1a
sususu25 <3985788@naver.com>
2025-06-20 15:00:09 +0900

    s3 url for list added

--- backend-services/product-service/index.js
+++ backend-services/product-service/index.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 const products = [
-    { id: 1, name: 'Monstera Deliciosa', price: 25, description: 'A beautiful and easy-to-care-for plant that is known for its iconic split leaves. It prefers bright, indirect light and watering every 1-2 weeks.', image: 'https://upload.wikimedia.org/wikipedia/commons/5/52/Monstera_deliciosa.png' },
-    { id: 2, name: 'Snake Plant', price: 20, description: 'A popular houseplant with stiff, upright leaves. It is very tolerant of low light and irregular watering.', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Snake_Plant_%28Sansevieria_trifasciata_%27Laurentii%27%29.jpg' },
-    { id: 3, name: 'Fiddle Leaf Fig', price: 45, description: 'A stylish plant with large, violin-shaped leaves. It needs bright, filtered light and consistent moisture.', image: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Starr_031108-0130_Ficus_lyrata.jpg' },
-    { id: 4, name: 'Pothos', price: 15, description: 'A forgiving and versatile plant with trailing, heart-shaped leaves. It can thrive in a variety of light conditions.', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Epipremnum_aureum_%28405626861%29.jpg' }
+    { id: 1, name: 'Monstera Deliciosa', price: 25, description: 'A beautiful and easy-to-care-for plant that is known for its iconic split leaves. It prefers bright, indirect light and watering every 1-2 weeks.', image: 'https://gogetyourgreen-images.s3.ap-northeast-2.amazonaws.com/Monstera.jpg' },
+    { id: 2, name: 'Snake Plant', price: 20, description: 'A popular houseplant with stiff, upright leaves. It is very tolerant of low light and irregular watering.', image: 'https://gogetyourgreen-images.s3.ap-northeast-2.amazonaws.com/snake-plant.jpg' },
+    { id: 3, name: 'Fiddle Leaf Fig', price: 45, description: 'A stylish plant with large, violin-shaped leaves. It needs bright, filtered light and consistent moisture.', image: 'https://gogetyourgreen-images.s3.ap-northeast-2.amazonaws.com/fiddle-leaf-fig.jpg' },
+    { id: 4, name: 'Pothos', price: 15, description: 'A forgiving and versatile plant with trailing, heart-shaped leaves. It can thrive in a variety of light conditions.', image: 'https://gogetyourgreen-images.s3.ap-northeast-2.amazonaws.com/pothos.jpg' }
 ];
 
 // Endpoint to get all products
